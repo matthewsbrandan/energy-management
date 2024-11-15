52115f6832926d074c91b54dd15500971fd60f9d
Mateus Brandão <mateusfleria@gmail.com>
2024-11-15 16:59:04 -0300

    feat: Tornando dinâmico a prop monitoramento no profile

--- src/domain/useCases/WebView/Profile/ProfileUseCase.ts
+++ src/domain/useCases/WebView/Profile/ProfileUseCase.ts
@@ -10,20 +10,33 @@ export class ProfileUseCase{
   ){}
 
   async execute({ user }:DTO){
-    const conecteds = await this.deviceRepo.count({
+    const devices = await this.deviceRepo.findAll({
       user_id: user.id
-    })
+    }, { include: { device_type: true }})
 
-    const actives = await this.deviceRepo.count({
-      user_id: user.id,
-      status: 'ativo'
-    })
-
+    const conecteds = devices.length;
+    let actives = 0;
     let monitoring = 0;
-    // const devices = await this.deviceRepo.findAll({
-    //   user_id: user.id
-    // })
+    let unit = undefined;
 
-    return { conecteds, actives, monitoring }
+    devices.forEach((device) => {
+      if(device.status === 'ativo') actives++;
+      if(!device.device_type?.data?.monitoring || !device.data?.monitoring?.date) return;
+
+      if(!unit) unit = device.data.monitoring.unit;
+      else if(device.data.monitoring.unit !== unit) return;
+
+      const now = new Date();
+
+      if([
+        now.getFullYear(),
+        String(now.getMonth() + 1).padStart(2,'0'),
+        String(now.getDate()).padStart(2,'0')
+      ].join('-') !== device.data.monitoring.date) return;
+
+      monitoring+= device.data.monitoring.total;
+    })
+
+    return { conecteds, actives, monitoring: `${monitoring}${unit}` }
   }
 }
